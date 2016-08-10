710ed0e4b0a435c628d445dcd0d03ec580b400d3
Techywarrior <techywarrior@gmail.com>
2016-08-10 09:27:21 -0700

    removed jQuery, fixed fullscreen display

--- background.js
+++ background.js
@@ -1,10 +1,20 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('window.html', {
-        //state: "fullscreen",
+    chrome.app.window.create('window.html', {
+        state: "fullscreen",
         'bounds': {
-			'width': screen.width,
-			'height': screen.height
-		}
-	});
+            'width': screen.width,
+            'height': screen.height
+        }
+    });
 
+});
+
+document.addEventListener('webkitfullscreenchange', function(){
+    if (chrome.app.window.current().isFullscreen()){
+        webview.style.height = screen.height + 'px';
+        webview.style.width = screen.width + 'px';
+    }
+    else{
+        //
+    }
 });
--- window.js
+++ window.js
@@ -1,14 +1,8 @@
-jQuery(function($) {
-	$(window).on("resize", resizeWindow);
-
-    //Emby hosted web client
-    var hosted_url = "http://app.emby.media";
-
-    $("webview").attr("src",hosted_url).show();
-    resizeWindow();
-
-});
-
-function resizeWindow() {
-	$("#webview").height($(window).height()-15).width($(window).width()).attr("width",$(window).width()).attr("height",$(window).height());
+function updateWebviews() {
+    var webview = document.querySelector("webview");
+    webview.style.height = document.documentElement.clientHeight + "px";
+    webview.style.width = document.documentElement.clientWidth + "px";
 }
+
+onload = updateWebviews;
+window.onresize = updateWebviews;
